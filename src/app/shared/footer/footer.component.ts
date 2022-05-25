a160dda71af254bd75735a1e13fb9055eb1e57e3
Santiago Valencia <esvalencia@integral.com.co>
2022-05-25 14:43:52 -0500

    colors basic integral header

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,10 +6,6 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  public flatlogic: string = 'https://flatlogic.com/'
-  public flatlogicAbout: string = 'https://flatlogic.com/about'
-  public flatlogicBlog: string = 'https://flatlogic.com/blog'
-
   constructor() {}
 
   ngOnInit() {}
