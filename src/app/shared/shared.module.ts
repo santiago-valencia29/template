35b17607f3a2edd2979ed0cf852800c5083f143c
Santiago Valencia <esvalencia@integral.com.co>
2022-05-25 09:57:34 -0500

    config formatter code

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,26 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { AngularMaterialModule } from '../app-material.module';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+import { AngularMaterialModule } from '../app-material.module'
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
 
 @NgModule({
-  declarations: [
-
-  ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule,
-    AngularMaterialModule,
-  ],
-  exports: [
-  ],
+  declarations: [],
+  imports: [CommonModule, FormsModule, RouterModule, AngularMaterialModule],
+  exports: [],
   providers: [
     { provide: MAT_DIALOG_DATA, useValue: {} },
-    { provide: MatDialogRef, useValue: {} },
-  ],
+    { provide: MatDialogRef, useValue: {} }
+  ]
 })
-export class SharedModule { }
+export class SharedModule {}
