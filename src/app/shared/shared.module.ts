6ceea685c015b40414c9d0a0dfc50ef494631b03
Santiago Valencia <esvalencia@integral.com.co>
2022-05-25 09:23:10 -0500

    add file angular material global

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AngularMaterialModule } from '../app-material.module';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    AngularMaterialModule,
+  ],
+  exports: [
+  ],
+  providers: [
+    { provide: MAT_DIALOG_DATA, useValue: {} },
+    { provide: MatDialogRef, useValue: {} },
+  ],
+})
+export class SharedModule { }
