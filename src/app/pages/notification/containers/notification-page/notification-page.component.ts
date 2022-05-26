b551d33454b4d8697efbcc32e2c984778f0b300d
Santiago Valencia <esvalencia@integral.com.co>
2022-05-26 18:12:06 -0500

    notifications sweetalert2

--- src/app/pages/notification/containers/notification-page/notification-page.component.ts
+++ src/app/pages/notification/containers/notification-page/notification-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-notification-page',
@@ -9,4 +10,80 @@ export class NotificationPageComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  error() {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Something went wrong!',
+      footer: '<a href="">Why do I have this issue?</a>'
+    })
+  }
+
+  warning() {
+    Swal.fire({
+      customClass: {
+        popup: 'large-sa-popup'
+      },
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire('Deleted!', 'Your file has been deleted.', 'success')
+      }
+    })
+  }
+
+  positionTop() {
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Your work has been saved',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
+
+  success() {
+    Swal.fire({
+      position: 'bottom-left',
+      icon: 'success',
+      title: 'Your work has been saved',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
+
+  info() {
+    Swal.fire({
+      position: 'top',
+      title: '<strong>HTML <u>example</u></strong>',
+      icon: 'info',
+      html:
+        'You can use <b>bold text</b>, ' +
+        '<a href="//sweetalert2.github.io">links</a> ' +
+        'and other HTML tags',
+      showCloseButton: true,
+      showCancelButton: true,
+      focusConfirm: false
+    })
+  }
+
+  toast() {
+    Swal.fire({
+      text: 'Toast with custom target',
+      target: '#custom-target',
+      customClass: {
+        container: 'position-absolute'
+      },
+      toast: true,
+      position: 'bottom-right',
+      timer: 1800
+    })
+  }
 }
