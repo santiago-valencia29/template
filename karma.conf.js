d5979d9988aba140a1b9a79635a12b2df58abedc
Santiago Valencia <esvalencia@integral.com.co>
2022-06-07 11:40:19 -0500

    change name project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/WebAngularPortalEmpleadosTest'),
+      dir: require('path').join(__dirname, './coverage/WebAngularPortalEmpleados'),
       subdir: '.',
       reporters: [
         { type: 'html' },
